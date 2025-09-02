c8be9a61bb0dc6ef35481bd44471510dd89a4fd5
Monster <lavlinskiy.i@mail.ru>
2025-09-02 20:02:38 +0300

    Сделал Header

--- components/header.php
+++ components/header.php
@@ -1,21 +1,68 @@
 <!-- Header -->
 <header class="header">
-    <div class="container">
-        <nav class="navigation">
-            <!-- Логотип -->
-            <div class="logo">
+    <div class="header__top">
+        <div class="container">
+            <span class="header__warning">Имеются противопоказания, необходимо проконсультироваться со специалистом 18+</span>
+        </div>
+    </div>
 
+    <div class="container">
+        <div class="header__main">
+            <div class="logo">
+                <img src="assets/images/logo/Logo.svg" alt="Логотип клиники" class="logo__image">
             </div>
-            
-            <!-- Меню навигации -->
-            <ul class="nav-menu">
-                
-            </ul>
-            
-            <!-- Контактная информация в шапке -->
-            <div class="header-contacts">
-                
+
+            <nav class="nav" aria-label="Основная навигация">
+                <ul class="nav__list">
+                    <li class="nav__item">
+                        <a href="#services" class="nav__link">Услуги</a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="#treatment" class="nav__link">Этапы лечения</a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="#about" class="nav__link">О нас</a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="#reviews" class="nav__link">Отзывы</a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="#contacts" class="nav__link">Контакты</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div class="contacts">
+                <a href="tel:+79061800041" class="contacts__phone">8 (906) 180-00-41</a>
+                <span class="contacts__schedule">Работаем 24/7</span>
             </div>
-        </nav>
+
+            <button class="button button--primary" type="button" data-modal="callback">
+                Заказать звонок
+            </button>
+        </div>
     </div>
 </header>
+
+<!-- Hero Section -->
+<section class="hero">
+    <div class="container">
+        <div class="hero__content">
+            <h1 class="hero__title">Срочный вызов врача-нарколога</h1>
+            <ul class="hero__features">
+                <li class="hero__feature">
+                    <p>Сохраняем анонимность, не требуем паспорт и не ставим на учёт</p>
+                </li>
+                <li class="hero__feature">
+                    <p>Помощь квалифицированного врача на дому</p>
+                </li>
+                <li class="hero__feature">
+                    <p>Приедем на адрес за 20 минут</p>
+                </li>
+                <li class="hero__feature">
+                    <p>Первичная консультация - бесплатно</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
