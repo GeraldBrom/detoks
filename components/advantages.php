ec465dd654f4aa835c781199ea6f725844c97815
Иван <lavlinskiy.i@mail.ru>
2025-09-03 12:50:52 +0300

    Добавил секцию "Преимущества" с описанием услуг и формой обратной связи. Обновил стили для кнопок и элементов секции, а также исправил ширину кнопки в секции "Услуги".

--- components/advantages.php
+++ components/advantages.php
@@ -1,6 +1,51 @@
 <!-- Advantages Section -->
 <section class="advantages">
     <div class="container">
-        
+        <div class="advantages__content">
+            <div class="advantages__image">
+                <img src="assets\images\adventages\doctorAdvantages.png" alt="Доктор" class="advantages__image-pic">
+            </div>
+            <div class="advantages__text">
+                <div class="advantages__header">
+                    <h3 class="advantages__title">Преимущества вызова нарколога</h3>
+                </div>
+                <div class="advantages__description">
+                    <p class="advantages__description-text">
+                        Обслуживание наркологом на дому имеет ряд плюсов, особенно для тех, кто боится огласки. 
+                        В этой ситуации обращение в частную наркологию максимально отвечает интересам пациента или его родственников. 
+                    </p>
+                </div>
+                <div class="advantages__benefits">
+                    <h4 class="advantages__benefits-title">Преимущества нашей клиники:</h4>
+                    <ul class="advantages__list">
+                        <li class="advantages__item">
+                            <p class="advantages__item-text">Нарколог на дом прибывает конфиденциально, автомобиль не имеет опознавательных знаков специализированного центра.</p>
+                        </li>
+                        <li class="advantages__item">
+                            <p class="advantages__item-text">Обрыв запойного состояния или снятие абстинентного синдрома проводятся под контролем профессионала, что исключает риски появления осложнений.</p>
+                        </li>
+                        <li class="advantages__item">
+                            <p class="advantages__item-text">Зависимый и его родственники бесплатно получают развернутую консультацию по окончании процедур. Если необходимо, доктор оставляет медикаменты на несколько дней приёма.</p>
+                        </li>
+                        <li class="advantages__item">
+                            <p class="advantages__item-text">Мы не передаем личных данных наших клиентов для постановки на учёт в наркологический диспансер. Обращение к нам для пациентов полностью анонимно.</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="advantages__form">
+            <div class="advantages__form-content">
+                <h4 class="advantages__form-title">Больной не хочет лечиться?</h4>
+                <p class="advantages__form-text">Врачами создана эффективная технология мотивации пациентов согласиться на терапию.</p>
+                <p class="advantages__form-text">Метод показывает высокие результаты — 8 из 10 людей добровольно решаются начать лечение от зависимости.</p>
+            </div>
+            <div class="advantages__form-input">
+                <input type="text" placeholder="Ваше имя">
+                <input type="tel" placeholder="Ваш телефон">
+                <button type="submit">Записаться</button>
+                <span class="advantages__form-text">Нажимая кнопку «Получить консультацию», вы соглашаетесь с политикой конфиденциальности</span>
+            </div>
+        </div>
     </div>
 </section>
